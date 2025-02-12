00215c38840796efa02a9abfb267f16ba353d6d4
shanhhe <shanhhe@gmail.com>
2025-02-12 16:54:54 +0000

    add fix camera

--- legged_gym/scripts/play.py
+++ legged_gym/scripts/play.py
@@ -22,10 +22,16 @@ def play(args):
     env_cfg.noise.add_noise = False
     env_cfg.domain_rand.randomize_friction = False
     env_cfg.domain_rand.push_robots = False
+    init_pos = [0.0, 0.0, 1.5]  # Initial position of the robot
+    init_ori = [0.0, 0.0, 0.0, 1.0]  # Initial orientation of the robot
+    env_cfg.init_state.pos = init_pos
+    env_cfg.init_state.rot = init_ori
 
     env_cfg.env.test = True
 
     # prepare environment
+    if MOVE_CAMERA:
+        args.num_envs=1
     env, _ = task_registry.make_env(name=args.task, args=args, env_cfg=env_cfg)
     obs = env.get_observations()
     # load policy
@@ -40,12 +46,25 @@ def play(args):
         print('Exported policy as jit script to: ', path)
 
     for i in range(10*int(env.max_episode_length)):
+        # print(env.base_pos)
+        if MOVE_CAMERA:
+            position = env.base_pos[0].clone()
+            position[2] += 2
+            position[0] += 2
+            position[1] += 2
+            # print(f'position: {position}')
+            lookat = env.base_pos[0]
+            # print(f'lookat', lookat)
+            env.set_camera(position, lookat)
+            # pos = [10, 0, 6]  # [m]
+            # lookat = [11., 5, 3.]  # [m]
+            # env.set_camera(pos, position)
         actions = policy(obs.detach())
         obs, _, rews, dones, infos = env.step(actions.detach())
 
 if __name__ == '__main__':
     EXPORT_POLICY = True
     RECORD_FRAMES = False
-    MOVE_CAMERA = False
+    MOVE_CAMERA = True
     args = get_args()
     play(args)
